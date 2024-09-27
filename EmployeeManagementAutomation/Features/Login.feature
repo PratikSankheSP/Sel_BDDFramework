f280609ca587c89e0e225938c8d103deb63fd63c
Pratik Sankhe <pratik.sankhe@sureprep.com>
2024-09-27 10:47:17 +0530

    Add project files.

---
+++ EmployeeManagementAutomation/Features/Login.feature
@@ -0,0 +1,15 @@
+﻿Feature: Login
+Need to lgin in system to manage employees records
+
+In order to Manage Employee Records
+As a HR
+I would like to access Orange HRM
+
+A short summary of the feature
+
+Scenario: ValidLogin
+	Given I have browser with OrageHRM application
+	When I enter username as 'Admin'
+	And I enter password as 'admin123'
+	And I click on login
+	Then I should get access to dashboard page with 'Quick Launch'
---
+++ EmployeeManagementAutomation/StepDefinitions/LoginStepDefinitions.cs
@@ -0,0 +1,40 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace EmployeeManagementAutomation.StepDefinitions
+{
+    [Binding]
+    public class LoginStepDefinitions
+    {
+
+        [Given(@"I have browser with OrageHRM application")]
+        public void GivenIHaveBrowserWithOrageHRMApplication()
+        {
+            Console.WriteLine("Given");
+        }
+
+        [When(@"I enter username as '(.*)'")]
+        public void WhenIEnterUsernameAs(string username)
+        {
+            Console.WriteLine("When"+username);
+        }
+
+        [When(@"I enter password as '(.*)'")]
+        public void WhenIEnterPasswordAs(string password)
+        {
+           Console.WriteLine("When"+password);
+        }
+
+        [When(@"I click on login")]
+        public void WhenIClickOnLogin()
+        {
+           
+        }
+
+        [Then(@"I should get access to dashboard page with '(.*)'")]
+        public void ThenIShouldGetAccessToDashboardPageWith(string message)
+        {
+           Console.WriteLine("Then"+message);
+        }
+    }
+}
